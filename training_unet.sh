21da4aaeacd5eb04bbdf1269e159706406c07666
fi263pnye <fi263pnye@login02.sc.uni-leipzig.de>
2024-06-12 16:49:29 +0200

    hcp status

---
+++ training_unet.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:v100:1
+#SBATCH --time=60
+#SBATCH --mem=100G
+#SBATCH --partition=clara
+#SBATCH --output=../log/%x.out-%j
+#SBATCH --error=../log/%x.error-%j
+#SBATCH --mail-type=END
+
+source ../ma_env/bin/activate
+
+module load Python/3.10.4-GCCcore-11.3.0
+module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
+
+
+python src/train_unet.py
