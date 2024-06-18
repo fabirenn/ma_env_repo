7aaaa30a570d58c937336019652e1e7f3b3e9f18
fabiorenn <fabio.renn@outlook.de>
2024-06-18 10:11:35 +0200

    added training=true

--- src/u_net/unet_architecture_hcp.py
+++ src/u_net/unet_architecture_hcp.py
@@ -3,7 +3,7 @@ from keras.layers import BatchNormalization
 
 
 def unet(
-    img_width, img_height, img_channels, batch_size, pretrained_weights=None
+    img_width, img_height, img_channels, batch_size, pretrained_weights=None, training=True
 ):
     # build the model
     inputs = tf.keras.layers.Input(
