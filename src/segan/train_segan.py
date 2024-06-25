ed93e79eefb7186888c773ac4e1e149fe99675e2
fabiorenn <fabio.renn@outlook.de>
2024-06-25 09:58:42 +0200

    added segan_test.py

--- src/segan/train_segan.py
+++ src/segan/train_segan.py
@@ -23,28 +23,28 @@ from data_loader import (
 
 os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
 
-'''
+
 TRAIN_IMG_PATH = "data/training_train/images_mixed"
 TRAIN_MASK_PATH = "data/training_train/labels_mixed"
 VAL_IMG_PATH = "data/training_val/images_mixed"
 VAL_MASK_PATH = "data/training_val/labels_mixed"
 TEST_IMG_PATH = "data/training_test/images_mixed"
-TEST_MASK_PATH = "data/training_test/labels_mixed"'''
+TEST_MASK_PATH = "data/training_test/labels_mixed"
 
-
+'''
 TRAIN_IMG_PATH = "data/local/train/images"
 TRAIN_MASK_PATH = "data/local/train/labels"
 VAL_IMG_PATH = "data/local/val/images"
 VAL_MASK_PATH = "data/local/val/labels"
 TEST_IMG_PATH = "data/local/test/images"
-TEST_MASK_PATH = "data/local/test/labels"
+TEST_MASK_PATH = "data/local/test/labels"'''
 
 
 LOG_VAL_PRED = "data/predictions/segan"
 CHECKPOINT_PATH = "./artifacts/models/segan/segan_checkpoint.h5"
 
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
+IMG_WIDTH = 512
+IMG_HEIGHT = 512
 IMG_CHANNEL = 8
 
 BATCH_SIZE = 4
