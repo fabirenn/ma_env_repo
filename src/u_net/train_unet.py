fc9dce944ca1d4c0613dcf3d1490909c7dc0f774
fabiorenn <fabio.renn@outlook.de>
2024-07-03 13:14:04 +0200

    tracking metrics now

--- src/u_net/train_unet.py
+++ src/u_net/train_unet.py
@@ -22,18 +22,18 @@ TRAIN_MASK_PATH = "data/training_train/labels_mixed"
 VAL_IMG_PATH = "data/training_val/images_mixed"
 VAL_MASK_PATH = "data/training_val/labels_mixed"
 
-
+'''
 TRAIN_IMG_PATH = "data/local/train/images"
 TRAIN_MASK_PATH = "data/local/train/labels"
 VAL_IMG_PATH = "data/local/val/images"
-VAL_MASK_PATH = "data/local/val/labels"
+VAL_MASK_PATH = "data/local/val/labels"'''
 
 LOG_VAL_PRED = "data/predictions/unet"
 
 CHECKPOINT_PATH = "artifacts/models/unet/unet_checkpoint.h5"
 
-IMG_WIDTH = 128
-IMG_HEIGHT = 128
+IMG_WIDTH = 512
+IMG_HEIGHT = 512
 
 IMG_CHANNEL = 8
 
