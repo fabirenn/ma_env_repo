7edd4db95c1046d3d1baad38ba1f7fe1a40c48c3
fabiorenn <fabio.renn@outlook.de>
2024-07-04 17:22:28 +0200

    small changes

--- src/image_segmentation.py
+++ src/image_segmentation.py
@@ -114,8 +114,8 @@ def compute_metrics(true_mask, pred_mask):
         p = keras.metrics.Precision()
         p.update_state(true_flat, pred_flat)
         precision = p.result()
-        specificity = specificity_score(true_mask.astype(np.float32), pred_mask.astype(np.float32))
-        dice = dice_score(true_mask, pred_mask)
+        specificity = specificity_score(true_mask, pred_mask)
+        dice = dice_score(true_mask.astype(np.int64), pred_mask.astype(np.int64))
 
         return precision, specificity, dice
 
