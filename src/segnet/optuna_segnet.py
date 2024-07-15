a1ee86237add1d5e4dbe8584e82b2d484f3062bb
fabiorenn <fabio.renn@outlook.de>
2024-07-15 15:25:33 +0200

    changed dropout to categorical hyperparameter

--- src/segnet/optuna_segnet.py
+++ src/segnet/optuna_segnet.py
@@ -1,19 +1,16 @@
 import os
 import sys
+import optuna
 
 import keras.metrics
-import optuna
-import tensorflow as tf
-from keras.callbacks import EarlyStopping
+from segnet_model import segnet
 from wandb.integration.keras import WandbMetricsLogger, WandbModelCheckpoint
-
 import wandb
+from keras.callbacks import EarlyStopping
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from unet_model import unet
-
 from custom_callbacks import ValidationCallback, dice_score, specificity_score
-from data_loader import create_datasets_for_unet_training
+from data_loader import create_datasets_for_segnet_training
 from loss_functions import combined_loss, dice_loss, iou_loss
 
 os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
@@ -23,36 +20,32 @@ TRAIN_MASK_PATH = "data/training_train/labels_mixed"
 VAL_IMG_PATH = "data/training_val/images_mixed"
 VAL_MASK_PATH = "data/training_val/labels_mixed"
 
-LOG_VAL_PRED = "data/predictions/unet"
-CHECKPOINT_PATH = "artifacts/models/unet/unet_checkpoint.h5"
+CHECKPOINT_PATH = "artifacts/models/segnet/segnet_checkpoint.h5"
+LOG_VAL_PRED = "data/predictions/segnet"
 
 IMG_WIDTH = 512
 IMG_HEIGHT = 512
-
+IMG_CHANNEL = 3
 EPOCHS = 200
 PATIENCE = 70
 
-os.environ["WANDB_DIR"] = "wandb/train_unet"
-
+os.environ["WANDB_DIR"] = "wandb/train_segnet"
 
 def objective(trial):
     # Hyperparameter tuning
     BATCH_SIZE = trial.suggest_categorical("batch_size", [4, 8, 12, 16])
-    IMG_CHANNEL = trial.suggest_categorical("img_channel", [3, 8])
-    DROPOUT_RATE = trial.suggest_categorical("dropout_rate", [0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
-    loss_function = trial.suggest_categorical(
-        "loss_function", ["combined_loss", "dice_loss", "iou_loss"]
-    )
+    DROPOUT_RATE = trial.suggest_uniform("dropout_rate", 0.0, 0.5)
+    loss_function = trial.suggest_categorical("loss_function", ["combined_loss", "dice_loss", "iou_loss"])
 
     # Map the loss function name to the actual function
     loss_function_map = {
         "combined_loss": combined_loss,
         "dice_loss": dice_loss,
-        "iou_loss": iou_loss,
+        "iou_loss": iou_loss
     }
 
     try:
-        train_dataset, val_dataset = create_datasets_for_unet_training(
+        train_dataset, val_dataset = create_datasets_for_segnet_training(
             directory_train_images=TRAIN_IMG_PATH,
             directory_train_masks=TRAIN_MASK_PATH,
             directory_val_images=VAL_IMG_PATH,
@@ -60,29 +53,19 @@ def objective(trial):
             img_width=IMG_WIDTH,
             img_height=IMG_HEIGHT,
             batch_size=BATCH_SIZE,
-            channel_size=IMG_CHANNEL,
         )
 
         wandb.init(
-            project="unet",
+            project="segnet",
             entity="fabio-renn",
             mode="offline",
-            name=f"train-unet-{trial.number}",
-            config={
-                "metric": "accuracy",
-                "epochs": EPOCHS,
-                "batch_size": BATCH_SIZE,
-            },
+            name=f"train-segnet-{trial.number}",
+            config={"metric": "accuracy", "epochs": EPOCHS, "batch_size": BATCH_SIZE},
             dir=os.environ["WANDB_DIR"],
         )
 
-        model = unet(
-            IMG_WIDTH,
-            IMG_HEIGHT,
-            IMG_CHANNEL,
-            BATCH_SIZE,
-            DROPOUT_RATE,
-            training=True,
+        model = segnet(
+            input_size=(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL), dropout_rate=DROPOUT_RATE
         )
 
         model.compile(
@@ -128,19 +111,16 @@ def objective(trial):
             ],
         )
 
-        val_loss = min(history.history["val_loss"])
+        val_loss = min(history.history['val_loss'])
         wandb.finish()
 
         return val_loss
     except tf.errors.ResourceExhaustedError:
-        print(
-            "Resource exhausted error caught. GPU may not have enough memory."
-        )
+        print("Resource exhausted error caught. GPU may not have enough memory.")
         return float("inf")
     except Exception as e:
         print(f"An exception occurred: {e}")
         return float("inf")
-
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="minimize")
