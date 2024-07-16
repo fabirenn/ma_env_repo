01528559281468352af73c3f25df96001ed64995
fabiorenn <fabio.renn@outlook.de>
2024-07-16 12:38:58 +0200

    unet optuna ready for experimental run

--- src/u_net/optuna_unet.py
+++ src/u_net/optuna_unet.py
@@ -25,10 +25,10 @@ VAL_MASK_PATH = "data/training_val/labels_mixed"
 LOG_VAL_PRED = "data/predictions/unet"
 CHECKPOINT_PATH = "artifacts/models/unet/unet_checkpoint.h5"
 
-IMG_WIDTH = 128
-IMG_HEIGHT = 128
+IMG_WIDTH = 512
+IMG_HEIGHT = 512
 
-EPOCHS = 50
+EPOCHS = 100
 PATIENCE = 30
 
 os.environ["WANDB_DIR"] = "wandb/train_unet"
