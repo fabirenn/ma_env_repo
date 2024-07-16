f73add8329da2ee6fdf9deaa72193fc0fc9fe026
fabirenn <fabirenn@gmail.com>
2024-07-16 16:00:45 +0200

    changes

--- training_unet.sh
+++ training_unet.sh
@@ -3,9 +3,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:v100:2
-#SBATCH --time=07:00:00
-#SBATCH --mem=200G
-#SBATCH --partition=clara
+#SBATCH --time=4-0
+#SBATCH --mem=400G
+#SBATCH --partition=clara-long	
 #SBATCH --output=../log/%x.out-%j
 #SBATCH --error=../log/%x.error-%j
 #SBATCH --mail-type=END
@@ -15,4 +15,5 @@ source ../ma_env/bin/activate
 module load Python/3.10.4-GCCcore-11.3.0
 module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
 
-python src/u_net/train_unet.py
+#python src/u_net/train_unet.py
+python src/u_net/optuna_unet.py
--- training_unet2.sh
+++ training_unet2.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:v100:2
-#SBATCH --time=08:00:00
+#SBATCH --time=03:00:00
 #SBATCH --mem=400G
 #SBATCH --partition=clara
 #SBATCH --output=../log/%x.out-%j
@@ -15,5 +15,5 @@ source ../ma_env/bin/activate
 module load Python/3.10.4-GCCcore-11.3.0
 module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
 
-#python src/segnet/train_segnet.py
-python src/segnet/optuna_segnet.py
+#python src/u_net/train_unet.py
+python src/u_net/optuna_unet.py
