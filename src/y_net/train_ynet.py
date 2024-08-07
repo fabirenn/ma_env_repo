85b20d3e11ee6b870ac16b2d9e8b06c8f687a5aa
fabiorenn <fabio.renn@outlook.de>
2024-08-07 12:16:28 +0200

    ready for training

--- src/y_net/train_ynet.py
+++ src/y_net/train_ynet.py
@@ -76,10 +76,10 @@ wandb.init(
 config = wandb.config
 
 # create model & start training it
-model = build_feature_extractor_for_pretraining(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL, DROPOUT_RATE)
-#semantic_extractor_model.load_weights(CHECKPOINT_PATH_PRETRAINED)
+semantic_extractor_model = build_feature_extractor_for_pretraining(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL, DROPOUT_RATE)
+semantic_extractor_model.load_weights(CHECKPOINT_PATH_PRETRAINED)
 
-#model = build_ynet_with_pretrained_semantic_extractor(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL, DROPOUT_RATE, semantic_extractor_model)
+model = build_ynet_with_pretrained_semantic_extractor(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL, DROPOUT_RATE, semantic_extractor_model)
 
 model.compile(
     optimizer="adam",
@@ -103,7 +103,7 @@ model.fit(
     callbacks=[
         WandbMetricsLogger(log_freq="epoch"),
         keras.callbacks.ModelCheckpoint(
-            filepath=CHECKPOINT_PATH_PRETRAINED,
+            filepath=CHECKPOINT_PATH_YNET,
             save_best_only=True,
             save_weights_only=False,
             monitor="val_loss",
