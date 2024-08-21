e969f167fe7d01d7e1056ee0ba4c39e8cb503153
fabiorenn <fabio.renn@outlook.de>
2024-08-21 11:18:00 +0200

    segnet ready for tuning

--- src/segnet/segnet_model.py
+++ src/segnet/segnet_model.py
@@ -3,22 +3,28 @@ from keras.layers import Activation, BatchNormalization, Conv2D, Dropout, Input
 from keras.models import Model
 import keras
 
-INITIALIZER = ""
+
 def segnet(input_size, dropout_rate, num_filters, kernel_size, activation, use_batchnorm, initializer_function):
-    
-    INITIALIZER = initializer_function
     inputs = Input(input_size)
     
     # Encoder
     pool_indices = []
     x = inputs
     for filters in num_filters:
-        x = Conv2D(filters, kernel_size, padding="same", kernel_initializer=get_initializer())(x)
+        if initializer_function == "he_normal":
+            initializer = keras.initializers.HeNormal()
+        elif initializer_function == "he_uniform":
+            initializer = keras.initializers.HeUniform()
+        x = Conv2D(filters, kernel_size, padding="same", kernel_initializer=initializer)(x)
         if use_batchnorm:
             x = BatchNormalization()(x)
         x = Activation(activation)(x)
         x = Dropout(dropout_rate)(x)
-        x = Conv2D(filters, kernel_size, padding="same", kernel_initializer=get_initializer())(x)
+        if initializer_function == "he_normal":
+            initializer = keras.initializers.HeNormal()
+        elif initializer_function == "he_uniform":
+            initializer = keras.initializers.HeUniform()
+        x = Conv2D(filters, kernel_size, padding="same", kernel_initializer=initializer)(x)
         if use_batchnorm:
             x = BatchNormalization()(x)
         x = Activation(activation)(x)
@@ -29,12 +35,20 @@ def segnet(input_size, dropout_rate, num_filters, kernel_size, activation, use_b
     # Decoder
     for filters, ind in zip(reversed(num_filters), reversed(pool_indices)):
         x = MaxUnpooling2D((2, 2))([x, ind])
-        x = Conv2D(filters, kernel_size, padding="same", kernel_initializer=get_initializer())(x)
+        if initializer_function == "he_normal":
+            initializer = keras.initializers.HeNormal()
+        elif initializer_function == "he_uniform":
+            initializer = keras.initializers.HeUniform()
+        x = Conv2D(filters, kernel_size, padding="same", kernel_initializer=initializer)(x)
         if use_batchnorm:
             x = BatchNormalization()(x)
         x = Activation(activation)(x)
         x = Dropout(dropout_rate)(x)
-        x = Conv2D(filters, kernel_size, padding="same", kernel_initializer=get_initializer())(x)
+        if initializer_function == "he_normal":
+            initializer = keras.initializers.HeNormal()
+        elif initializer_function == "he_uniform":
+            initializer = keras.initializers.HeUniform()
+        x = Conv2D(filters, kernel_size, padding="same", kernel_initializer=initializer)(x)
         if use_batchnorm:
             x = BatchNormalization()(x)
         x = Activation(activation)(x)
@@ -46,12 +60,3 @@ def segnet(input_size, dropout_rate, num_filters, kernel_size, activation, use_b
     model = Model(inputs, outputs, name="SegNet")
     model.summary()
     return model
-
-
-def get_initializer():
-    if INITIALIZER == "he_normal":
-        return keras.initializers.HeNormal()
-    elif INITIALIZER == "he_uniform":
-        return keras.initializers.HeUniform()
-    else:
-        return ""
