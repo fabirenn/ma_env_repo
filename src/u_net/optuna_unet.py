cba20e032c024fdcb14e0e89b864200eb3519948
fabiorenn <fabio.renn@outlook.de>
2024-08-26 14:30:12 +0200

    small change

--- src/u_net/optuna_unet.py
+++ src/u_net/optuna_unet.py
@@ -123,13 +123,23 @@ def objective(trial, train_images, train_masks, val_images, val_masks):
         )
         val_loss = min(history.history["val_loss"])
         current_epoch = len(history.history["loss"])
+        print(f"Training completed. Final Validation Loss: {val_loss}")
+
+        trial.report(val_loss, step=current_epoch)
+        print("Reported to Optuna.")
+
+        if trial.should_prune():
+            print("Trial is pruned.")
+            raise optuna.TrialPruned()
+        
         return val_loss
     except tf.errors.ResourceExhaustedError as e:
         handle_errors_during_tuning(trial=trial, best_loss=val_loss, e=e, current_epoch=current_epoch)
         return float("inf")
-    except Exception as e:
-        handle_errors_during_tuning(trial=trial, best_loss=val_loss, e=e, current_epoch=current_epoch)
-        return float("inf")
+    finally:
+        # Clear GPU memory
+        keras.backend.clear_session()
+        print("Cleared GPU memory after trial.")
 
 
 def handle_errors_during_tuning(trial, best_loss, e, current_epoch):
