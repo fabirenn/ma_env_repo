8ac4ff2d3316b35e6676eff8a206c127cce6edee
fabiorenn <fabio.renn@outlook.de>
2024-08-26 13:52:56 +0200

    changed maxunpooling2d

--- src/segnet/custom_layers.py
+++ src/segnet/custom_layers.py
@@ -9,43 +9,33 @@ class MaxUnpooling2D(Layer):
         self.pool_size = pool_size
 
     def call(self, inputs):
-        updates, mask = inputs[0], inputs[1]
+        updates, mask = inputs
         input_shape = K.shape(updates)
-        mask = tf.cast(mask, "int32")
-
-        # Calculate output shape
         output_shape = [
             input_shape[0],
             input_shape[1] * self.pool_size[0],
             input_shape[2] * self.pool_size[1],
             input_shape[3],
         ]
+        output_shape = tf.stack(output_shape)
 
-        # Calculate indices for scatter
-        one_like_mask = tf.ones_like(mask, dtype="int32")
-        batch_shape = tf.concat([[input_shape[0]], [1], [1], [1]], axis=0)
-        batch_range = tf.reshape(
-            tf.range(output_shape[0], dtype="int32"), shape=batch_shape
-        )
-        b = one_like_mask * batch_range
+        flat_output_shape = [output_shape[0] * output_shape[1] * output_shape[2] * output_shape[3]]
+        
+        flat_input_size = input_shape[1] * input_shape[2] * input_shape[3]
 
-        y = mask // (output_shape[2] * output_shape[3])
-        x = (mask // output_shape[3]) % output_shape[2]
-        feature_range = tf.range(output_shape[3], dtype="int32")
-        f = one_like_mask * feature_range
+        batch_range = tf.reshape(tf.range(input_shape[0], dtype=mask.dtype), shape=[-1, 1, 1, 1])
+        b = tf.ones_like(mask) * batch_range
+        b = tf.reshape(b, [-1])
 
-        updates_size = tf.size(updates)
-        indices = tf.transpose(
-            tf.reshape(tf.stack([b, y, x, f]), [4, updates_size])
-        )
-        values = tf.reshape(updates, [updates_size])
+        flat_mask = tf.reshape(mask, [-1])
+        flat_mask += (b * flat_input_size)
 
-        # Ensure indices are within bounds
-        output_shape_tensor = tf.convert_to_tensor(output_shape, dtype=tf.int32)
-        indices = tf.clip_by_value(indices, 0, output_shape_tensor - 1)
+        flat_updates = tf.reshape(updates, [-1])
+        flat_output = tf.zeros(flat_output_shape, dtype=updates.dtype)
 
-        ret = tf.scatter_nd(indices, values, output_shape)
+        flat_output = tf.tensor_scatter_nd_add(flat_output, tf.expand_dims(flat_mask, 1), flat_updates)
 
+        ret = tf.reshape(flat_output, output_shape)
         return ret
 
     def compute_output_shape(self, input_shape):
