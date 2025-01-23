4cca5df07b054eb046373498f7c706011beeefaf
fabiorenn <fabio.renn@outlook.de>
2025-01-23 14:33:38 +0100

    templated adjusted

--- hcp_script_template.sh
+++ hcp_script_template.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:v100:2
+#SBATCH --mem=256GB
+#SBATCH --job-name="tune-segan"
+#SBATCH --time=10-00:00:00
+#SBATCH --partition=clara-long
+#SBATCH --output=../log/%x.out-%j
+#SBATCH --error=../log/%x.error-%j
+#SBATCH --mail-type=END
+
+source ../ma_env/bin/activate
+
+module load Python/3.10.4-GCCcore-11.3.0
+module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
+
+python src/segan/optuna_segan.py
